66a85e17b8180dbcf4bbc6ec36d9ceed027b0344
blinkous <saradhani1@gmail.com>
2020-08-04 12:05:11 -0400

    Updates

---
+++ precache-manifest.fd47a5d70cf694cb47b48439f82ac9f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1826c3e210c450e31a3146d4da0cc015",
+    "url": "/helix-sleep-challenge-2020/index.html"
+  },
+  {
+    "revision": "b5b043607c1be54cf49f",
+    "url": "/helix-sleep-challenge-2020/static/css/main.226132e2.chunk.css"
+  },
+  {
+    "revision": "4590f3bbc49e7302a1ff",
+    "url": "/helix-sleep-challenge-2020/static/js/2.a076560b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helix-sleep-challenge-2020/static/js/2.a076560b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b043607c1be54cf49f",
+    "url": "/helix-sleep-challenge-2020/static/js/main.b689ed71.chunk.js"
+  },
+  {
+    "revision": "5642d78b641682d75e23",
+    "url": "/helix-sleep-challenge-2020/static/js/runtime-main.3d9389f3.js"
+  }
+]);
